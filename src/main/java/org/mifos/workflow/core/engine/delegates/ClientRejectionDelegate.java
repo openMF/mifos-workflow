7df7bcd5150c90485adaf4625f40809d8a2b63d3
Hossam Hatem <7ossam7atem1@gmail.com>
2025-07-08 20:08:21 +0300

    feat: Add ClientRejectionDelegate for workflow integration

---
+++ src/main/java/org/mifos/workflow/core/engine/delegates/ClientRejectionDelegate.java
@@ -0,0 +1,28 @@
+package org.mifos.workflow.core.engine.delegates;
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Delegate for rejecting a client in the Fineract system.
+ * This is a placeholder implementation.
+ * Full implementation will be provided later.
+ */
+public class ClientRejectionDelegate implements JavaDelegate {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientRejectionDelegate.class);
+
+    @Override
+    public void execute(DelegateExecution execution) {
+        // TODO: Implement client rejection logic
+        logger.info("ClientRejectionDelegate.execute() called - placeholder implementation");
+        logger.info("Process instance ID: {}", execution.getProcessInstanceId());
+        logger.info("Variables: {}", execution.getVariables());
+
+        String clientId = (String) execution.getVariable("clientId");
+        String rejectionReason = (String) execution.getVariable("rejectionReason");
+        logger.info("Would reject client with ID: {} for reason: {}", clientId, rejectionReason);
+    }
+} 
