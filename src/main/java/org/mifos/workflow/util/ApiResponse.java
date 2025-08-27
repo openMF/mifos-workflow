543a3337274b1543335d3ff7465daf4d998ff220
Hossam Hatem <7ossam7atem1@gmail.com>
2025-08-27 15:31:37 +0300

    chore: add @Getter lombok annotation to ApiResponse

--- src/main/java/org/mifos/workflow/util/ApiResponse.java
+++ src/main/java/org/mifos/workflow/util/ApiResponse.java
@@ -1,6 +1,7 @@
 package org.mifos.workflow.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 
 import java.time.Instant;
 
@@ -8,6 +9,7 @@ import java.time.Instant;
  * Generic API response wrapper.
  * @param <T> the type of the response data
  */
+@Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
 
@@ -34,32 +36,16 @@ public class ApiResponse<T> {
         return new ApiResponse<>(true, message, data, Instant.now());
     }
 
-    public boolean isSuccess() {
-        return success;
-    }
-
     public void setSuccess(boolean success) {
         this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
     public void setMessage(String message) {
         this.message = message;
     }
 
-    public T getData() {
-        return data;
-    }
-
     public void setData(T data) {
         this.data = data;
-    }
-
-    public Instant getTimestamp() {
-        return timestamp;
     }
 
     public void setTimestamp(Instant timestamp) {
